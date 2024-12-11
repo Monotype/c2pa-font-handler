6089e56a22f8b8b07e5d1cd2ced89098bdcb1a30
Michael Brogdon <michael.brogdon@monotype.com>
2024-12-10 17:46:48 -0700

    Formatting changes for license comments.

--- c2pa-font-handler/examples/stub_dsig.rs
+++ c2pa-font-handler/examples/stub_dsig.rs
@@ -1,11 +1,11 @@
 // Copyright 2024 Monotype Imaging Inc.
-//  
+//
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
 //  You may obtain a copy of the License at
-//  
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//  
+//
 //  Unless required by applicable law or agreed to in writing, software
 //  distributed under the License is distributed on an "AS IS" BASIS,
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -42,7 +42,8 @@ async fn main() -> Result<(), anyhow::Error> {
     // Open the input file
     let mut input_file = std::fs::File::open(&args.input)?;
     // Read the font file
-    let mut font = c2pa_font_handler::sfnt::font::SfntFont::from_reader(&mut input_file)?;
+    let mut font =
+        c2pa_font_handler::sfnt::font::SfntFont::from_reader(&mut input_file)?;
     // Stub the DSIG table
     font.stub_dsig()?;
     // Open the output file
