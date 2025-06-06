c51171690fbce712ff3749d8f718dc9cf05b0d2d
Michael Brogdon <michael.brogdon@monotype.com>
2025-06-06 16:10:28 -0600

    C2PA-687: Ability to render thumbnails for SFNT files
    * Bring over the SVG Thumbnail Generator.
    
    * Bring in the PNG thumbnail generator.
    
    * Turn on the `svg-thumbnails` feature by default.
    
    * Update documentation
    
    * Fix mispelling.
    
    * Add a benchmark for thumbnail generation.
    
    * Add automock to the dictionary.
    
    * Make sure to catch the license exclusions.
    
    * Update the MSRV in the GH workflow
    
    * Update copyrights to current year
    
    * Address some issues in the code.
    
    * Update dependencies and update deny.toml to relax multiple versions
    
    ---------
    
    Co-authored-by: monotype-pitest <monotype-pitest@monotype.com>

---
+++ c2pa-font-handler/benches/thumbnails.rs
@@ -0,0 +1,70 @@
+// Copyright 2025 Monotype Imaging Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#![allow(missing_docs)]
+//! Benchmarks for thumbnail generation and handling in C2PA fonts.
+
+#[path = "utils/profiler.rs"]
+mod profiler_utils;
+use std::io::Cursor;
+
+use c2pa_font_handler::thumbnail::{
+    CosmicTextThumbnailGenerator, PngThumbnailRenderer,
+    PngThumbnailRendererConfig, SvgThumbnailRenderer,
+    SvgThumbnailRendererConfig, ThumbnailGenerator,
+};
+use criterion::{criterion_group, criterion_main, Criterion};
+use profiler_utils::DhatProfiler;
+
+/// Collection of benchmarks for SFNT font thumbnail generation.
+fn sfnt_thumbnail_benchmarks(c: &mut Criterion) {
+    let font_data = include_bytes!("../../.devtools/font.otf");
+
+    // Benchmark for generating an SVG thumbnail
+    c.bench_function("sfnt_svg_thumbnail_default", |b| {
+        b.iter(|| {
+            let mut font_stream = Cursor::new(font_data);
+            // SVG renderer
+            let svg_renderer = Box::new(SvgThumbnailRenderer::new(
+                SvgThumbnailRendererConfig::default(),
+            ));
+            let generator = CosmicTextThumbnailGenerator::new(svg_renderer);
+            let _ = generator
+                .create_thumbnail_from_stream(&mut font_stream)
+                .unwrap();
+        });
+    });
+
+    // Benchmark for generating a PNG thumbnail
+    c.bench_function("sfnt_png_thumbnail_default", |b| {
+        b.iter(|| {
+            let mut font_stream = Cursor::new(font_data);
+            // PNG renderer
+            let png_renderer = Box::new(PngThumbnailRenderer::new(
+                PngThumbnailRendererConfig::default(),
+            ));
+            let generator = CosmicTextThumbnailGenerator::new(png_renderer);
+            let _ = generator
+                .create_thumbnail_from_stream(&mut font_stream)
+                .unwrap();
+        });
+    });
+}
+
+criterion_group!(
+    name = benches;
+    config = Criterion::default().with_profiler(DhatProfiler::new());
+    targets =  sfnt_thumbnail_benchmarks,
+);
+criterion_main!(benches);
---
+++ c2pa-font-handler/src/thumbnail/error.rs
@@ -0,0 +1,58 @@
+// Copyright 2025 Monotype Imaging Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+//! Thumbnail error types.
+
+/// Errors that can occur when creating a font thumbnail
+#[derive(Debug, thiserror::Error)]
+#[non_exhaustive]
+pub enum FontThumbnailError {
+    /// Error from the image crate
+    #[cfg(feature = "png-thumbnails")]
+    #[error(transparent)]
+    ImageError(#[from] image::ImageError),
+    /// error from IO operations
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+    /// A font was not found
+    #[error("No font found")]
+    NoFontFound,
+    /// No full name found in the font
+    #[error("No full name found")]
+    NoFullNameFound,
+    /// Error when the buffer size is invalid
+    #[error("The buffer size is invalid")]
+    InvalidBufferSize,
+    #[cfg(feature = "svg-thumbnails")]
+    /// Could not create a Rect from the given values
+    #[error("Invalid values for Rect")]
+    InvalidRect,
+    /// Failed to find a point size that would accommodate the width and text
+    #[error("Failed to find an appropriate font point size to fit the width")]
+    FailedToFindAppropriateSize,
+    /// Failed to create a Pixmap object
+    #[error("Failed to create pixmap")]
+    FailedToCreatePixmap,
+    /// Failed to get a pixel from the image
+    #[error("Failed to get pixel from image; x: {x}, y: {y}")]
+    FailedToGetPixel { x: u32, y: u32 },
+    /// Failed to create a SVG tree from string
+    #[cfg(feature = "svg-thumbnails")]
+    #[error("Failed to create a SVG tree from string: {0}")]
+    FailedToCreateSvgTree(String),
+    #[cfg(not(feature = "svg-thumbnails"))]
+    /// The SVG feature is not enabled
+    #[error("The SVG feature is not enabled")]
+    SvgFeatureNotEnabled,
+}
